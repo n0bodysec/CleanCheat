658734a9c2a28606c459b2ba5b39262bf82d19ce
Nobody <git@n0bodysec.com>
2023-04-17 17:48:58 -0300

    fix: check LOG macro redefinition

--- CleanCheat/Macros.h
+++ CleanCheat/Macros.h
@@ -11,11 +11,13 @@
 
 #define __FILENAME__ (strrchr(__FILE__, '\\') ? strrchr(__FILE__, '\\') + 1 : __FILE__)
 
-#if USE_LOGGER && !defined(LOG)
+#ifndef LOG
+#if USE_LOGGER
 #include <cstdio>
 #include <cstring>
 #include <cstdio>
 #define LOG(format, ...); std::printf("[%s:%s:%i] " format "\n", __FILENAME__, __FUNCTION__, __LINE__, __VA_ARGS__);
 #else
 #define LOG(format, ...);
-#endif
+#endif // USE_LOGGER
+#endif // !LOG
